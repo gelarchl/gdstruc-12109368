426306851d2d5da6dcf74f91744bc9d1074e714d
Angela Albelda <angelarachel.albelda@benilde.edu.ph>
2025-03-06 22:57:59 +0800

    main.java

--- Midterms/main.java
+++ Midterms/main.java
@@ -1,92 +1,83 @@
 import java.util.Random;
 import java.util.Scanner;
 
-class Main {
-    private static final int DECK_SIZE = 30;
-    private static final String[] CARD_NAMES = {"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
+public class CardGame {
+    private static final int TOTAL_CARDS = 30;
+    private static final String[] CARD_TYPES = {"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
 
-        // Initialize player deck with 30 cards
-        CardStack playerDeck = new CardStack();
-        for (int i = 0; i < DECK_SIZE; i++) {
-            String cardName = CARD_NAMES[random.nextInt(CARD_NAMES.length)];
-            playerDeck.push(new Card(cardName));
+        CardStack deck = new CardStack();
+        for (int i = 0; i < TOTAL_CARDS; i++) {
+            String cardType = CARD_TYPES[rand.nextInt(CARD_TYPES.length)];
+            deck.push(new Card(cardType));
         }
 
-        // Initialize empty hand and discard pile
-        CardStack playerHand = new CardStack();
-        CardStack discardPile = new CardStack();
+        CardStack hand = new CardStack();
+        CardStack discard = new CardStack();
 
-        // Game loop
-        while (!playerDeck.isEmpty()) {
+        while (!deck.isEmpty()) {
             System.out.println("\n--- New Turn ---");
 
-            // Randomly select a command
-            int command = random.nextInt(3);
-            int x = random.nextInt(5) + 1; // Random number of cards (1 to 5)
+            int action = rand.nextInt(3);
+            int numCards = rand.nextInt(5) + 1;
 
-            switch (command) {
+            switch (action) {
                 case 0 -> {
-                    System.out.println("Command: Draw " + x + " cards");
-                    drawCards(playerDeck, playerHand, x);
+                    System.out.println("Action: Draw " + numCards + " cards");
+                    drawFromDeck(deck, hand, numCards);
                 }
                 case 1 -> {
-                    System.out.println("Command: Discard " + x + " cards");
-                    discardCards(playerHand, discardPile, x);
+                    System.out.println("Action: Discard " + numCards + " cards");
+                    discardFromHand(hand, discard, numCards);
                 }
                 case 2 -> {
-                    System.out.println("Command: Get " + x + " cards from the discard pile");
-                    getCardsFromDiscard(discardPile, playerHand, x);
+                    System.out.println("Action: Retrieve " + numCards + " cards from discard pile");
+                    retrieveFromDiscard(discard, hand, numCards);
                 }
             }
 
-            // Display game state
-            System.out.println("\nPlayer Hand: " + playerHand);
-            System.out.println("Remaining Cards in Deck: " + playerDeck.size());
-            System.out.println("Cards in Discard Pile: " + discardPile.size());
+            System.out.println("\nPlayer Hand: " + hand);
+            System.out.println("Cards Remaining in Deck: " + deck.size());
+            System.out.println("Cards in Discard Pile: " + discard.size());
 
-            // Wait for player to press Enter
-            System.out.println("\nPress Enter to proceed to the next turn...");
-            scanner.nextLine();
+            System.out.println("\nPress Enter to continue to the next turn...");
+            input.nextLine();
         }
 
-        System.out.println("\nGame Over! The player deck is empty.");
+        System.out.println("\nGame Over! The deck is empty.");
     }
 
-    // Draw x cards from the deck to the hand
-    private static void drawCards(CardStack deck, CardStack hand, int x) {
-        for (int i = 0; i < x; i++) {
+    private static void drawFromDeck(CardStack deck, CardStack hand, int numCards) {
+        for (int i = 0; i < numCards; i++) {
             if (!deck.isEmpty()) {
                 hand.push(deck.pop());
             } else {
-                System.out.println("No more cards in the deck to draw!");
+                System.out.println("The deck is empty! No more cards to draw.");
                 break;
             }
         }
     }
 
-    // Discard x cards from the hand to the discard pile
-    private static void discardCards(CardStack hand, CardStack discardPile, int x) {
-        for (int i = 0; i < x; i++) {
+    private static void discardFromHand(CardStack hand, CardStack discard, int numCards) {
+        for (int i = 0; i < numCards; i++) {
             if (!hand.isEmpty()) {
-                discardPile.push(hand.pop());
+                discard.push(hand.pop());
             } else {
-                System.out.println("No more cards in the hand to discard!");
+                System.out.println("Your hand is empty! No more cards to discard.");
                 break;
             }
         }
     }
 
-    // Get x cards from the discard pile to the hand
-    private static void getCardsFromDiscard(CardStack discardPile, CardStack hand, int x) {
-        for (int i = 0; i < x; i++) {
-            if (!discardPile.isEmpty()) {
-                hand.push(discardPile.pop());
+    private static void retrieveFromDiscard(CardStack discard, CardStack hand, int numCards) {
+        for (int i = 0; i < numCards; i++) {
+            if (!discard.isEmpty()) {
+                hand.push(discard.pop());
             } else {
-                System.out.println("No more cards in the discard pile to get!");
+                System.out.println("The discard pile is empty! No more cards to retrieve.");
                 break;
             }
         }
